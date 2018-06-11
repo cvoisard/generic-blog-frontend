df685f3ca194404e90f40b5a256e0e3013a1d6a2
Colin <colin.voisard@yahoo.com>
2018-06-10 20:28:14 -0400

    commit files

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { PostService } from '../../services/post-service.service';
-import { Post } from '../../models/post';
 
 @Component({
   selector: 'app-post',
@@ -10,13 +7,10 @@ import { Post } from '../../models/post';
 })
 export class PostComponent implements OnInit {
 
-  posts$: Observable<Post[]>;
 
-  constructor(private postService: PostService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.posts$ = this.postService.getPosts();
-    this.posts$.subscribe(res => console.log(res));
   }
 
 }
